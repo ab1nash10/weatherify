9c6bd9e95e7f94a98f5965b16b243ed280563d32
ab1nash10 <ab1nashkumar9815@gmail.com>
2024-02-13 01:35:53 +0530

    commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Weatherify
+
+This is a single page weather application that allows users to search for current weather conditions in a city of their choice. It displays weather details like temperature, humidity, wind speed and description.
+
+## Tech Stack
+
+**Thunder Client:** HTML, CSS, JavaScript, API
+
+## Features
+
+Search for weather by city name  
+Display current temperature, humidity, wind speed, and description for searched city  
+Clear and visually appealing UI showing weather details and icons
+
+## APIs
+
+OpenWeatherMap - used to retrieve weather data by city name  
+API key required with free tier allowance of 60 calls/minute
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/ab1nash10/weatherify.git
+```
+
+Go to the project directory
+
+```bash
+  cd weatherify
+```
+
+Open with VSCode
+
+```bash
+  code .
+```
+
+Open with live server
---
+++ src/files/script.js
@@ -0,0 +1,62 @@
+/* These lines of code are declaring and initializing variables using the `const` keyword. Each
+variable is assigned a reference to a specific HTML element on the webpage using the `querySelector`
+method. */
+const apiKey = "124f56f7736f5662c56a82975872ed64";
+const form = document.querySelector("form");
+const weather = document.querySelector(".weather");
+const search = document.querySelector("#searchbox");
+const city = document.querySelector(".city");
+const temp = document.querySelector(".temp");
+const wind = document.querySelector(".windtxt");
+const humidity = document.querySelector(".humiditytxt");
+const visibility = document.querySelector(".visibilitytxt");
+const feelsLike = document.querySelector(".feelsLike");
+const icon = document.querySelector(".imgWeather");
+/**
+ * The `getWeather` function fetches weather data from the OpenWeatherMap API for a given city and
+ * displays the weather description, icon, and other information on a webpage.
+ * @param cityName - The `cityName` parameter is a string that represents the name of the city for
+ * which you want to retrieve the weather information.
+ */
+const getWeather = async (cityName) => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  const weatherDes = data.weather;
+  weatherDes.forEach((element) => {
+    feelsLike.innerHTML = element.description;
+    path = element.icon;
+    icon.innerHTML = `
+    <img src="../../assets/images/weather_icons/${path}.png" alt="" class="h-10" />`;
+  });
+  showWeather(data);
+  console.log(data);
+  const date = new Date();
+  city.innerHTML = `
+  <h1 class="cityName">${data.name}</h1>
+  <h2 class="date h-auto w-full rounded-md p-2">${date.toUTCString()}</h2>
+  `;
+};
+
+/**
+ * The function `showWeather` updates the HTML elements with weather data and removes the "hidden"
+ * class to display the elements.
+ * @param data - The `data` parameter is an object that contains weather information. It is expected to
+ * have the following properties:
+ */
+const showWeather = (data) => {
+  search.value = "";
+  temp.classList.remove("hidden");
+  temp.textContent = data.main.temp;
+  wind.classList.remove("hidden");
+  wind.textContent = data.wind.speed;
+  humidity.classList.remove("hidden");
+  humidity.textContent = data.main.humidity;
+  visibility.classList.remove("hidden");
+  visibility.textContent = data.visibility / 1000;
+  feelsLike.classList.remove("hidden");
+};
+form.addEventListener("submit", (e) => {
+  getWeather(search.value);
+  e.preventDefault();
+});
